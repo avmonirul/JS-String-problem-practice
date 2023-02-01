f066008faee3a8e53338ed7db4d82dcb064f3c52
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-02-02 00:17:52 +0600

    first commit

--- case.js
+++ case.js
@@ -1,1 +1,9 @@
-const userName = 'black pink';
+const userName = 'blackpink';
+const userInput = 'blackpinK';
+
+if (userName === userInput){
+    console.log('valid user')
+}
+else{
+    console.log('invalid user')
+}
