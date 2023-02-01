9e806b0af6f8d869e57b4dad4a7946bda6b843df
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-02-02 00:33:47 +0600

    first commit

--- case.js
+++ case.js
@@ -1,6 +1,8 @@
 const userName = 'blackpink';
 const userInput = 'blackpinK';
 console.log(userName.toLowerCase());
+console.log(userInput.toUpperCase());
+console.log(userName.toLocaleLowerCase());
 if (userName.toLocaleLowerCase() === userInput.toLocaleLowerCase()){
     console.log('valid user')
 }
