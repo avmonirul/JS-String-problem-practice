207839a6d80fd82bd3c540d34141c9da6141da47
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-02-02 23:32:37 +0600

    first commit

--- tallest.js
+++ tallest.js
@@ -1,7 +1,11 @@
 function maxInArray(number){
-
+for(let i = 0; i < number.length; i++){
+    const index = i;
+    const element =number[index];
+    console.log(element);
+    }
 }
 
 const heights = [165, 190, 120, 165, 137];
 const tallest = maxInArray(heights);
-console.log(tallest);
+console.log('tallest person is', tallest);
